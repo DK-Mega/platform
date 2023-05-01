e650e9ffc96710e0c67d7a8ed316cb61fd71bf04
JonasMagalhaes <jonasevmagalhaes@gmail.com>
2023-05-01 11:02:42 -0300

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: 'dashboard', loadChildren: () => import('dashboard/Module').then(m => m.MainModule)}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
